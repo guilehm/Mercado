574172dc48b0e15df5fc959508d0daf385d3f6be
guile.hm <guile.hm@hotmail.com>
2018-04-08 00:27:29 -0300

    Model created, Admin registered

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente
+from .models import Produto
+from .models import Pedido
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Produto)
+admin.site.register(Pedido)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    cliente     = models.CharField('Nome Cliente', max_length=15)
+    cpf         = models.CharField('CPF', max_length=11)
+
+    def __str__(self):
+        return self.cliente
+
+
+class Produto(models.Model):
+    produto     = models.CharField('Nome Produto', max_length=20)
+    descricao   = models.TextField('Descrição', max_length=50)
+    preco       = models.DecimalField('Preço', decimal_places=2, max_digits=7)
+
+    def __str__(self):
+        return self.nome
+
+
+class Pedido(models.Model):
+    cliente     = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    criado      = models.DateField('Criado em', auto_now_add=True)
+    modificado  = models.DateField('Modificado em', auto_now_add=False, auto_now=True)
