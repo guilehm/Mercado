b0a3aee9e83df1fe0aca6baf7e41e456bbd93308
guile.hm <guile.hm@hotmail.com>
2018-04-13 16:30:42 -0300

    Inline formset html changes

--- core/forms.py
+++ core/forms.py
@@ -14,3 +14,13 @@ class DetalhePedidoForm(forms.ModelForm):
     class Meta:
         model = DetalhePedido
         fields = '__all__'
+
+
+    def __init__(self, *args, **kwargs):
+        super(DetalhePedidoForm, self).__init__(*args, **kwargs)
+        self.fields['preco'].widget.attrs['class'] = 'form-control form-control-sm'
+        self.fields['preco'].widget.attrs['min'] = '0.01'
+        self.fields['preco'].widget.attrs['step'] = '0.1'
+        self.fields['produto'].widget.attrs['class'] = 'form-control form-control-sm'
+        self.fields['quantidade'].widget.attrs['class'] = 'form-control form-control-sm'
+        self.fields['quantidade'].widget.attrs['min'] = '0'
