f18f4bd9236412a956dcb93e147ea98dbd7b3ad8
guile.hm <guile.hm@hotmail.com>
2018-04-24 22:55:25 -0300

    admin updates

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Cliente
 from .models import Produto
 from .models import Pedido
+from .models import DetalhePedido
 
 # Register your models here.
 
@@ -11,6 +12,15 @@ class PedidoAdmin(admin.ModelAdmin):
     list_filter = ['cliente', ]
     search_fields = ['cliente__cliente', 'id']
 
+class DetalhePedidoAdmin(admin.ModelAdmin):
+    list_display = ['pedido', 'produto', 'quantidade','preco', ]
+
+    list_filter = ['pedido', 'produto','preco' ]
+    search_fields = ['pedido', 'produto','preco']
+
+
+
 admin.site.register(Cliente)
 admin.site.register(Produto)
+admin.site.register(DetalhePedido, DetalhePedidoAdmin)
 admin.site.register(Pedido, PedidoAdmin)
